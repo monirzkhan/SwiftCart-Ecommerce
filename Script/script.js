384cd59ffa9b6b73afc90d3e9b87e1204a0c5e4c
monirzkhan <odvutkabbo@gmail.com>
2026-02-17 00:48:52 +0600

    slice done

--- Script/script.js
+++ Script/script.js
@@ -8,23 +8,11 @@ const loadTrendingProducts=async()=>{
 }
 
 
-// {
-//     "id": 20,
-//     "title": "DANVOUY Womens T Shirt Casual Cotton Short",
-//     "price": 12.99,
-//     "description": "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
-//     "category": "women's clothing",
-//     "image": "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_t.png",
-//     "rating": {
-//         "rate": 3.6,
-//         "count": 145
-//     }
-// }
 const displayTrendingProducts=(products)=>{
      const productTrendingContainer=document.getElementById('trending-container');
     productTrendingContainer.innerHTML="";
     
-    products.forEach(product => {
+    products.slice(0,3).forEach(product => {
     console.log(product);
 
     const trendingCard=document.createElement('div');
@@ -32,13 +20,11 @@ const displayTrendingProducts=(products)=>{
     
 <div class="">
 
-<div class="card bg-base-100 w-96 h-96 shadow-sm ">
-        <figure>
-        <img
-      src="${product.image}"
-      class="h-full" alt="Shoes" />
+<div class="card bg-base-100 w-96 h-[420px] shadow-lg ">
+        <figure class="bg-gray-300 h-96">
+        <img src="${product.image}" class="h-full py-4"/>
+      </figure>
       
-        </figure>
         <div>
         <div class="py-2 px-4 flex justify-between">
             <div>
@@ -46,7 +32,7 @@ const displayTrendingProducts=(products)=>{
             </div>
              <div>
             <span class="text-yellow-500"><i class="fa-solid fa-star"></i></span>
-            <span>3${product.rating.rate}</span>
+            <span>${product.rating.rate}</span>
             <span>(${product.rating.count})</span>
         </div>
         </div>
