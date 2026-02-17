2cfe163c455d3939a2654e5481c251d12e20e04c
monirzkhan <odvutkabbo@gmail.com>
2026-02-17 17:21:47 +0600

    product details modal

--- Script/script.js
+++ Script/script.js
@@ -46,10 +46,68 @@ const loadAllProducts=async()=>{
     displayAllProducts(data);
     
 }
+// {
+//     "id": 1,
+//     "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+//     "price": 109.95,
+//     "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+//     "category": "men's clothing",
+//     "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+//     "rating": {
+//         "rate": 3.9,
+//         "count": 120
+//     }
+// }
 
+const loadProductDetails=async(id)=>{
+    const url=`https://fakestoreapi.com/products/${id}`;
+    const res= await fetch(url);
+    const data= await res.json();
+    displayProductDetails(data);
+
+}
+
+const displayProductDetails=(product)=>{
+    const detailsContainer=document.getElementById('details-container');
+    detailsContainer.innerHTML=`
+    <div class="card bg-base-100 w-96 py-2 shadow-lg ">
+      
+      
+        <div>
+        <div class="py-2 px-4 flex justify-between">
+            <div>
+                <a class="btn bg-blue-100 rounded-full text-blue-600 ">${product.category}</a>
+            </div>
+             <div>
+            <span class="text-yellow-500"><i class="fa-solid fa-star"></i></span>
+            <span>${product.rating.rate}</span>
+            <span>(${product.rating.count})</span>
+        </div>
+        </div>
+       
+      </div>
+        <div class="card-body">
+         <h2 class="card-title">
+         ${product.title}
+            </h2>
+             <h3 class="font-semibold ">
+         ${product.description}
+            </h3>
+        <p class="text-bold text-2xl text-left">$ ${product.price}</p>
+        <div class="card-actions justify-between">
+        <div onclick="loadProductDetails(${product.id})" class="btn btn-active py-4 w-5/12 rounded-lg"><i class="fa-solid fa-bag-shopping"></i>Buy Now</div>
+        <div class="btn btn-active btn-primary w-5/12 rounded-lg"><i class="fa-solid fa-cart-shopping"></i>Add To Cart</div>
+        </div>
+        </div>
+        </div>
+    
+    `;
+    document.getElementById('product_modal').showModal();
+   
+}
 const displayAllProducts=(products)=>{
      const productsContainer=document.getElementById('products-container');
-    productsContainer.innerHTML=" ";
+    productsContainer.innerHTML='';
     
     products.forEach(product => {
 
@@ -82,7 +140,7 @@ const displayAllProducts=(products)=>{
             </h2>
         <p class="text-bold text-2xl text-left">$ ${product.price}</p>
         <div class="card-actions justify-between">
-        <div class="btn btn-active py-4 w-5/12 rounded-lg"><i class="fa-regular fa-eye"></i>Details</div>
+        <div onclick="loadProductDetails(${product.id})" class="btn btn-active py-4 w-5/12 rounded-lg"><i class="fa-regular fa-eye"></i>Details</div>
         <div class="btn btn-active btn-primary w-5/12 rounded-lg"><i class="fa-solid fa-cart-shopping"></i>Add</div>
         </div>
         </div>
@@ -111,7 +169,7 @@ const loadTrendingProducts=async()=>{
 
 const displayTrendingProducts=(products)=>{
      const productTrendingContainer=document.getElementById('trending-container');
-    productTrendingContainer.innerHTML=" ";
+    productTrendingContainer.innerHTML='';
     
     products.slice(0,3).forEach(product => {
     console.log(product);
