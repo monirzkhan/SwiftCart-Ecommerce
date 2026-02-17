4e4280903fecb3f5c3039ca80afed59d6e22cb97
monirzkhan <odvutkabbo@gmail.com>
2026-02-17 15:16:14 +0600

    product display by category

--- Script/script.js
+++ Script/script.js
@@ -1,28 +1,105 @@
 
+
+
+
 const loadCategoriesButton=()=>{
     const url='https://fakestoreapi.com/products/categories';
     fetch(url)
     .then(res=>res.json())
     .then(data=>displayCategoriesButton(data))
 }
+
+const loadByCategory=async(id)=>{
+ const url=`https://fakestoreapi.com/products/category/${encodeURIComponent(id)}`;
+ const res= await fetch(url);
+ const data= await res.json();
+ displayAllProducts(data);
+
+}
+
 const displayCategoriesButton=(categories)=>{
 const categoriesContainer=document.getElementById('product-categories');
 categoriesContainer.innerHTML=`<button class="btn py-4 bg-white shadow-sm  rounded-lg">All</button>`;
 
 categories.forEach(category=>{
-    console.log(category)
-const btnDiv=document.createElement('div');
-btnDiv.innerHTML=`
+const btn = document.createElement('button');
+    btn.className = "btn py-4 bg-white shadow-sm rounded-lg";
+    btn.textContent = category[0].toUpperCase() + category.slice(1);
+    btn.dataset.category = category;
 
-<button class="btn py-4 bg-white shadow-sm rounded-lg">${category}</button>
-`
+    btn.addEventListener("click", () => {
+      loadByCategory(category);
+    });
 
-categoriesContainer.append(btnDiv)
+    categoriesContainer.appendChild(btn);
 })
 
 }
 loadCategoriesButton()
 
+// ----------------------------------
+const loadAllProducts=async()=>{
+    const url='https://fakestoreapi.com/products';
+
+    const res= await fetch (url);
+    const data= await res.json();
+    displayAllProducts(data);
+    
+}
+
+const displayAllProducts=(products)=>{
+     const productsContainer=document.getElementById('products-container');
+    productsContainer.innerHTML=" ";
+    
+    products.forEach(product => {
+
+    const productCard=document.createElement('div');
+    productCard.innerHTML=`
+    
+<div class="">
+
+<div class="card bg-base-100 w-96 h-[420px] shadow-lg ">
+        <figure class="bg-gray-300 h-96">
+        <img src="${product.image}" class="h-full py-4"/>
+      </figure>
+      
+        <div>
+        <div class="py-2 px-4 flex justify-between">
+            <div>
+                <a class="btn bg-blue-100 rounded-full text-blue-600 ">${product.category}</a>
+            </div>
+             <div>
+            <span class="text-yellow-500"><i class="fa-solid fa-star"></i></span>
+            <span>${product.rating.rate}</span>
+            <span>(${product.rating.count})</span>
+        </div>
+        </div>
+       
+      </div>
+        <div class="card-body">
+         <h2 class="card-title">
+         ${product.title}
+            </h2>
+        <p class="text-bold text-2xl text-left">$ ${product.price}</p>
+        <div class="card-actions justify-between">
+        <div class="btn btn-active py-4 w-5/12 rounded-lg"><i class="fa-regular fa-eye"></i>Details</div>
+        <div class="btn btn-active btn-primary w-5/12 rounded-lg"><i class="fa-solid fa-cart-shopping"></i>Add</div>
+        </div>
+        </div>
+        </div>
+
+</div>
+
+    `
+    productsContainer.append(productCard);
+        
+    });
+    
+}
+loadAllProducts();
+
+
+// ----------------------------------------
 const loadTrendingProducts=async()=>{
     const url='https://fakestoreapi.com/products';
 
@@ -34,7 +111,7 @@ const loadTrendingProducts=async()=>{
 
 const displayTrendingProducts=(products)=>{
      const productTrendingContainer=document.getElementById('trending-container');
-    productTrendingContainer.innerHTML="";
+    productTrendingContainer.innerHTML=" ";
     
     products.slice(0,3).forEach(product => {
     console.log(product);
