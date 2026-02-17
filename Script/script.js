6be34443b82e887592d608725759a10858fe00ee
monirzkhan <odvutkabbo@gmail.com>
2026-02-17 23:22:29 +0600

    trending details

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,4 @@
 
-
-
-
 const loadCategoriesButton=()=>{
     const url='https://fakestoreapi.com/products/categories';
     fetch(url)
@@ -28,6 +25,7 @@ const removeActive=()=>{
     categoryBtn.forEach(btn=>btn.classList.remove('active'));
     
 }
+
 const truncateText = (text, limit = 30) => {
   return text.length > limit
     ? text.slice(0, limit) + "..."
@@ -47,15 +45,16 @@ const displayCategoriesButton=(categories)=>{
 const categoriesContainer=document.getElementById('product-categories');
 
     categoriesContainer.innerHTML = `
-    <button class="btn py-4 bg-white shadow-sm rounded-lg category-btn active" id="all-btn">All </button>
-`;
+    <button class="btn py-4 bg-white shadow-sm rounded-lg category-btn active" id="all-btn">
+    All 
+    </button> `;
 
     document.getElementById("all-btn").addEventListener("click", () => {
     removeActive();
     document.getElementById("all-btn").classList.add("active");
     loadAllProducts();
     
-});
+    });
 
 categories.forEach(category=>{
 const btn = document.createElement('button');
@@ -86,12 +85,12 @@ const loadAllProducts=async()=>{
     displayAllProducts(data);   
 }
 
-
 const loadProductDetails=async(id)=>{
     const url=`https://fakestoreapi.com/products/${id}`;
     const res= await fetch(url);
     const data= await res.json();
     displayProductDetails(data);
+    
 }
 
 const displayProductDetails=(product)=>{
@@ -121,17 +120,16 @@ const displayProductDetails=(product)=>{
             </h3>
         <p class="text-bold text-2xl text-left">$ ${product.price}</p>
         <div class="card-actions justify-between">
-        <div class="btn btn-active py-4 w-5/12 rounded-lg"><i class="fa-solid fa-bag-shopping"></i>Buy Now</div>
-        <div class="btn btn-active btn-primary w-5/12 rounded-lg"><i class="fa-solid fa-cart-shopping"></i>Add To Cart</div>
+        <div class="btn  bg-white py-4 w-5/12 rounded-lg"><i class="fa-solid fa-bag-shopping"></i>Buy Now</div>
+        <div class="btn btn-primary w-5/12 rounded-lg"><i class="fa-solid fa-cart-shopping"></i>Add To Cart</div>
         </div>
         </div>
-        </div>
-
-    `;
+        </div> 
+        `;
     document.getElementById('product_modal').showModal();
-   
     
 }
+
 const displayAllProducts=(products)=>{
     
     const productsContainer=document.getElementById('products-container');
@@ -161,23 +159,22 @@ const displayAllProducts=(products)=>{
         </div>
         </div>
        
-      </div>
+        </div>
         <div class="card-body">
          <h2 class="card-title">
          ${truncateText(product.title, 30)}
             </h2>
         <p class="text-bold text-2xl text-left">$ ${product.price}</p>
         <div class="card-actions justify-between">
-        <div onclick="loadProductDetails(${product.id})" class="btn btn-active py-4 w-5/12 rounded-lg"><i class="fa-regular fa-eye"></i>Details</div>
+        <div onclick="loadProductDetails(${product.id})" class="btn  bg-white py-4 w-5/12 rounded-lg"><i class="fa-regular fa-eye"></i>Details</div>
         <div class="btn btn-active btn-primary w-5/12 rounded-lg"><i class="fa-solid fa-cart-shopping"></i>Add</div>
         </div>
         </div>
         </div>
 
-</div>
+        </div>
 
     `
-    
     productsContainer.append(productCard);
      
     });
@@ -187,6 +184,8 @@ loadAllProducts();
 
 
 // ----------------------------------------
+
+
 const loadTrendingProducts=async()=>{
     const url='https://fakestoreapi.com/products';
 
@@ -195,6 +194,51 @@ const loadTrendingProducts=async()=>{
     displayTrendingProducts(data);
     
 }
+
+const loadProductTrending=async(id)=>{
+    const url=`https://fakestoreapi.com/products/${id}`;
+    const res= await fetch(url);
+    const data= await res.json();
+    displayTrendingProductDetails(data);
+}
+const displayTrendingProductDetails=(product)=>{
+    console.log(product);
+    const detailsContainer2=document.getElementById('details-container-2');
+    detailsContainer2.innerHTML=`
+    <div class="card bg-base-100 w-96 py-2 shadow-lg ">
+        <div>
+        <div class="py-2 px-4 flex justify-between">
+            <div>
+                <a class="btn bg-blue-100 rounded-full text-blue-600 ">${product.category}</a>
+            </div>
+             <div>
+            <span class="text-yellow-500"><i class="fa-solid fa-star"></i></span>
+            <span>${product.rating.rate}</span>
+            <span>(${product.rating.count})</span>
+        </div>
+        </div>
+       
+      </div>
+        <div class="card-body">
+         <h2 class="card-title">
+         ${product.title}
+            </h2>
+             <h3 class="font-semibold ">
+         ${product.description}
+            </h3>
+        <p class="text-bold text-2xl text-left">$ ${product.price}</p>
+        <div class="card-actions justify-between">
+        <div class="btn  bg-white py-4 w-5/12 rounded-lg"><i class="fa-solid fa-bag-shopping"></i>Buy Now</div>
+        <div class="btn btn-primary w-5/12 rounded-lg"><i class="fa-solid fa-cart-shopping"></i>Add To Cart</div>
+        </div>
+        </div>
+        </div> 
+        `;
+    document.getElementById('my_modal_3').showModal();
+}
+
+
+
 
 const displayTrendingProducts=(products)=>{
      const productTrendingContainer=document.getElementById('trending-container');
@@ -232,14 +276,13 @@ const displayTrendingProducts=(products)=>{
             </h2>
         <p class="text-bold text-2xl text-left">$ ${product.price}</p>
         <div class="card-actions justify-between">
-        <div onclick="loadProductDetails(${product.id})" class="btn btn-active py-4 w-5/12 rounded-lg"><i class="fa-regular fa-eye"></i>Details</div>
+        <div onclick="loadProductTrending(${product.id})" class="btn bg-white py-4 w-5/12 rounded-lg"><i class="fa-regular fa-eye"></i>Details</div>
         <div class="btn btn-active btn-primary w-5/12 rounded-lg"><i class="fa-solid fa-cart-shopping"></i>Add</div>
         </div>
         </div>
         </div>
 
-</div>
-
+        </div>
     `
     productTrendingContainer.append(trendingCard);
         
